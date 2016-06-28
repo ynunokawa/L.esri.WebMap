592c984a07ff57d7623ee0d2ae8e2cde94ab55c0
ynunokawa <yusuke_nunokawa@esrij.com>
2016-06-28 14:39:43 +0900

    es6
    use ES6 modules (import/export)

---
+++ src/EsriLeafletWebMap.js
@@ -0,0 +1,7 @@
+// import base
+export { WebMap, webMap } from './WebMapLoader';
+export { operationalLayer } from './OperationalLayer';
+
+// export tasks
+import { createPopupContent } from './Popup/Popup';
+import { createLabelText } from './Label/Label';
