f4038c8a0a6c6b419822ea3e3c07ed532fdfaee3
ynunokawa <yusuke_nunokawa@esrij.com>
2016-07-02 09:53:44 +0900

    single run for karma

--- karma.conf.js
+++ karma.conf.js
@@ -69,12 +69,12 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity,
-    
+
     // Configure the coverage reporters
     coverageReporter: {
       instrumenters: {
