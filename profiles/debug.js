d2b8a8b53f7ad2de02865d53a5903935de45b09d
ynunokawa <yusuke_nunokawa@esrij.com>
2016-07-07 15:16:20 +0900

    remove files expect for demos

--- profiles/debug.js
+++
@@ -1,6 +0,0 @@
-import config from './base.js';
-
-config.dest = 'dist/esri-leaflet-webmap-debug.js';
-config.sourceMap = 'inline';
-
-export default config;
--- src/EsriLeafletWebMap.js
+++
@@ -1,7 +0,0 @@
-// export base
-export { WebMap, webMap } from './WebMapLoader';
-
-// export sub-modules
-export { operationalLayer } from './OperationalLayer';
-export { createPopupContent } from './Popup/Popup';
-export { createLabelText } from './Label/Label';
