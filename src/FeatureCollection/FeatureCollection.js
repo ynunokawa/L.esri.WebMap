1a0bacf6a329578255ceefba21052cbb6c9255d0
ynunokawa <yusuke_nunokawa@esrij.com>
2016-07-22 13:34:28 +0900

    use arcgis-to-geojson-utils as converter

--- src/FeatureCollection/FeatureCollection.js
+++ src/FeatureCollection/FeatureCollection.js
@@ -1,5 +1,6 @@
 import L from 'leaflet';
 
+import { arcgisToGeoJSON } from 'arcgis-to-geojson-utils';
 import { classBreaksRenderer, uniqueValueRenderer, simpleRenderer } from 'esri-leaflet-renderers';
 
 export var FeatureCollection = L.GeoJSON.extend({
@@ -46,14 +47,70 @@ export var FeatureCollection = L.GeoJSON.extend({
   _parseFeatureCollection: function (data) {
     var features = data.layers[0].featureSet.features;
     var geometryType = data.layers[0].layerDefinition.geometryType; // 'esriGeometryPoint' | 'esriGeometryMultipoint' | 'esriGeometryPolyline' | 'esriGeometryPolygon' | 'esriGeometryEnvelope'
-    var geojson = this._featureCollectionToGeoJSON(features, geometryType);
+    var objectIdField = data.layers[0].layerDefinition.objectIdField;
+
+    if (data.layers[0].layerDefinition.extent.spatialReference.wkid === 102100) {
+      features = this._projTo4326(features, geometryType);
+    }
+
+    var geojson = this._featureCollectionToGeoJSON(features, objectIdField);
 
     this._setRenderers(data.layers[0].layerDefinition);
+    console.log(geojson);
     this.addData(geojson);
   },
 
-  _featureCollectionToGeoJSON: function (features, geometryType) {
-    var geojson = {
+  _projTo4326: function (features, geometryType) {
+    console.log('_project!');
+    var i, len;
+    var projFeatures = [];
+
+    for (i = 0, len = features.length; i < len; i++) {
+      var f = features[i];
+      var mercatorToLatlng;
+      var j, k;
+
+      if (geometryType === 'esriGeometryPoint') {
+        mercatorToLatlng = L.Projection.SphericalMercator.unproject(L.point(f.geometry.x, f.geometry.y));
+        f.geometry.x = mercatorToLatlng.lng;
+        f.geometry.y = mercatorToLatlng.lat;
+      } else if (geometryType === 'esriGeometryMultipoint') {
+        var plen;
+
+        for (j = 0, plen = f.geometry.points.length; j < plen; j++) {
+          mercatorToLatlng = L.Projection.SphericalMercator.unproject(L.point(f.geometry.points[j][0], f.geometry.points[j][1]));
+          f.geometry.points[j][0] = mercatorToLatlng.lng;
+          f.geometry.points[j][1] = mercatorToLatlng.lat;
+        }
+      } else if (geometryType === 'esriGeometryPolyline') {
+        var pathlen, pathslen;
+
+        for (j = 0, pathslen = f.geometry.paths.length; j < pathslen; j++) {
+          for (k = 0, pathlen = f.geometry.paths[j].length; k < pathlen; k++) {
+            mercatorToLatlng = L.Projection.SphericalMercator.unproject(L.point(f.geometry.paths[j][k][0], f.geometry.paths[j][k][1]));
+            f.geometry.paths[j][k][0] = mercatorToLatlng.lng;
+            f.geometry.paths[j][k][1] = mercatorToLatlng.lat;
+          }
+        }
+      } else if (geometryType === 'esriGeometryPolygon') {
+        var ringlen, ringslen;
+
+        for (j = 0, ringslen = f.geometry.rings.length; j < ringslen; j++) {
+          for (k = 0, ringlen = f.geometry.rings[j].length; k < ringlen; k++) {
+            mercatorToLatlng = L.Projection.SphericalMercator.unproject(L.point(f.geometry.rings[j][k][0], f.geometry.rings[j][k][1]));
+            f.geometry.rings[j][k][0] = mercatorToLatlng.lng;
+            f.geometry.rings[j][k][1] = mercatorToLatlng.lat;
+          }
+        }
+      }
+      projFeatures.push(f);
+    }
+
+    return projFeatures;
+  },
+
+  _featureCollectionToGeoJSON: function (features, objectIdField) {
+    var geojsonFeatureCollection = {
       type: 'FeatureCollection',
       features: []
     };
@@ -61,82 +118,13 @@ export var FeatureCollection = L.GeoJSON.extend({
     var i, len;
 
     for (i = 0, len = features.length; i < len; i++) {
-      var f;
-      var mercatorToLatlng, coordinates;
-      var j, k;
-
-      if (geometryType === 'esriGeometryPoint') {
-        mercatorToLatlng = L.Projection.SphericalMercator.unproject(L.point(features[i].geometry.x, features[i].geometry.y));
-        coordinates = [mercatorToLatlng.lng, mercatorToLatlng.lat];
-
-        f = {
-          type: 'Feature',
-          geometry: { type: 'Point', coordinates: coordinates },
-          properties: features[i].attributes
-        };
-      } else if (geometryType === 'esriGeometryMultipoint') {
-        var plen;
-        var points = [];
-
-        for (j = 0, plen = features[i].geometry.points.length; j < plen; j++) {
-          mercatorToLatlng = L.Projection.SphericalMercator.unproject(L.point(features[i].geometry.points[j][0], features[i].geometry.points[j][1]));
-          coordinates = [mercatorToLatlng.lng, mercatorToLatlng.lat];
-          points.push(coordinates);
-        }
-
-        f = {
-          type: 'Feature',
-          geometry: { type: 'MultiPoint', coordinates: points },
-          properties: features[i].attributes
-        };
-      } else if (geometryType === 'esriGeometryPolyline') {
-        var pathlen, pathslen;
-        var paths = [];
-
-        for (j = 0, pathslen = features[i].geometry.paths.length; j < pathslen; j++) {
-          var path = [];
-          for (k = 0, pathlen = features[i].geometry.paths[j].length; k < pathlen; k++) {
-            mercatorToLatlng = L.Projection.SphericalMercator.unproject(L.point(features[i].geometry.paths[j][k][0], features[i].geometry.paths[j][k][1]));
-            coordinates = [mercatorToLatlng.lng, mercatorToLatlng.lat];
-            path.push(coordinates);
-          }
-          paths.push(path);
-        }
-
-        f = {
-          type: 'Feature',
-          geometry: { type: 'MultiLineString', coordinates: paths },
-          properties: features[i].attributes
-        };
-      } else if (geometryType === 'esriGeometryPolygon') {
-        var ringlen, ringslen;
-        var rings = [];
-
-        for (j = 0, ringslen = features[i].geometry.rings.length; j < ringslen; j++) {
-          var ring = [];
-          for (k = 0, ringlen = features[i].geometry.rings[j].length; k < ringlen; k++) {
-            mercatorToLatlng = L.Projection.SphericalMercator.unproject(L.point(features[i].geometry.rings[j][k][0], features[i].geometry.rings[j][k][1]));
-            coordinates = [mercatorToLatlng.lng, mercatorToLatlng.lat];
-            ring.push(coordinates);
-          }
-          rings.push(ring);
-        }
-
-        f = {
-          type: 'Feature',
-          geometry: { type: 'MultiPolygon', coordinates: rings },
-          properties: features[i].attributes
-        };
-      } else if (geometryType === 'esriGeometryEnvelope') {
-
-      }
-
-      featuresArray.push(f);
+      var geojson = arcgisToGeoJSON(features[i], objectIdField);
+      featuresArray.push(geojson);
     }
 
-    geojson.features = featuresArray;
+    geojsonFeatureCollection.features = featuresArray;
 
-    return geojson;
+    return geojsonFeatureCollection;
   },
 
   _checkForProportionalSymbols: function (geometryType, renderer) {
