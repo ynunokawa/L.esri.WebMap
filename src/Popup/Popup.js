715874700ddd291d06ac69b631711aa1ded15c12
ynunokawa <yusuke_nunokawa@esrij.com>
2016-09-08 11:15:09 +0900

    support kml popup

--- src/Popup/Popup.js
+++ src/Popup/Popup.js
@@ -15,17 +15,25 @@ export function createPopupContent (popupInfo, properties) {
 
   content = '<div class="leaflet-popup-content-title"><h4>' + titleText + '</h4></div><div class="leaflet-popup-content-description" style="max-height:200px;overflow:auto;">';
 
-  for (var i = 0; i < popupInfo.fieldInfos.length; i++) {
-    if (popupInfo.fieldInfos[i].visible === true) {
-      content += '<div style="font-weight:bold;color:#999;margin-top:5px;word-break:break-all;">' + popupInfo.fieldInfos[i].label + '</div><p style="margin-top:0;margin-bottom:5px;word-break:break-all;">' + properties[popupInfo.fieldInfos[i].fieldName] + '</p>';
+  if (popupInfo.fieldInfos !== undefined) {
+    for (var i = 0; i < popupInfo.fieldInfos.length; i++) {
+      if (popupInfo.fieldInfos[i].visible === true) {
+        content += '<div style="font-weight:bold;color:#999;margin-top:5px;word-break:break-all;">' + popupInfo.fieldInfos[i].label + '</div><p style="margin-top:0;margin-bottom:5px;word-break:break-all;">' + properties[popupInfo.fieldInfos[i].fieldName] + '</p>';
+      }
     }
+    content += '</div>';
+  } else if (popupInfo.description !== undefined) {
+    // KMLLayer popup
+    var descriptionText = popupInfo.description.replace(r, function (s) {
+      var m = r.exec(s);
+      return properties[m[1]];
+    });
+    content += descriptionText + '</div>'
   }
 
-  content += '</div>';
-
-  if (popupInfo.mediaInfos.length > 0) {
+  //if (popupInfo.mediaInfos.length > 0) {
     // It does not support mediaInfos for popup contents.
-  }
+  //}
 
   return content;
 }
